0786a02d263e0d7829c6b95b1ef1c7ca46e4d5ca
Meck Zhu <yesmeck@gmail.com>
2020-05-07 20:31:29 +0800

    1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import flattenChildren from './flattenChildren';
 
-export * as React from 'react';
+export {
+  Children,
+  createRef,
+  Component,
+  PureComponent,
+  createContext,
+  forwardRef,
+  lazy,
+  memo,
+  useCallback,
+  useContext,
+  useEffect,
+  useImperativeHandle,
+  useDebugValue,
+  useLayoutEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+  Fragment,
+  Profiler,
+  StrictMode,
+  Suspense,
+  createElement,
+  cloneElement,
+  isValidElement,
+  version,
+  createFactory,
+} from 'react';
 
 export const shared = {
   flattenChildren
